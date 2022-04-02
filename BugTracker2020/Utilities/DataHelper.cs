fcf7db67ac557b7ab4d573b6fca6971f81a41a5e
Denis Jojot <denis.jojot.ferreira@gmail.com>
2022-04-02 10:46:52 -0400

    Added SSL Required

--- BugTracker2020/Utilities/DataHelper.cs
+++ BugTracker2020/Utilities/DataHelper.cs
@@ -40,7 +40,9 @@ namespace BugTracker2020.Utilities
         Port = databaseUri.Port,
         Username = userInfo[0],
         Password = userInfo[1],
-        Database = databaseUri.LocalPath.TrimStart('/')
+        Database = databaseUri.LocalPath.TrimStart('/'),
+        SslMode = SslMode.Require,
+        TrustServerCertificate = true
       };
       return builder.ToString();
     }
